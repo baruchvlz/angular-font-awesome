415d57c06cc81c2595db2dfcbab07e9bc6fd2a13
baruchvlz <baruchvelez@gmail.com>
2017-01-08 11:47:24 -0500

    chore(demo) demo for basic usage

--- demo/src/app/app.component.spec.ts
+++ demo/src/app/app.component.spec.ts
@@ -19,16 +19,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Angular2 Font Awesome Examples');
   }));
 });
