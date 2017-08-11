ef948c755be82fc717b279e8ee931aadf1b2e7fa
baruchvlz <baruchvelez@gmail.com>
2017-08-11 23:46:15 +0200

    update demo

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular2 Font Awesome Examples';
+  title = 'Angular Font Awesome Examples';
 }
