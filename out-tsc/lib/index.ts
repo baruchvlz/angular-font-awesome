06768d1c028082afdeefc393f8527dcab158cd80
Baruch Velez <baruchvelez@gmail.com>
2017-11-06 00:51:29 +0100

    hotfix(3.0.2): fix npm package published files

--- out-tsc/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export { AngularFontAwesomeComponent } from './src/component/angular-font-awesome.component';
-export { AngularFontAwesomeService } from './src/service/angular-font-awesome.service';
-export { AngularFontAwesomeModule } from './src/angular-font-awesome.module';
